ba6aea33b8d7b292d929d63d65b6b94e4d206ec8
azuell <86937359+azuell@users.noreply.github.com>
2023-11-08 23:18:29 +1100

    Adapt splash screen for mobile and tablet, stop repeated sound effect, add favicon

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-
+var displaySplash = true;
 
 function fadeSplash() {
-    $(".splash-h1").fadeOut("slow");
+    if (displaySplash) {
+        $(".splash-h1").fadeOut("slow");
 
-    setTimeout(function() {
-        var audio = new Audio("./assets/gameboy_startup_audio.mp3");
-        audio.play();
-        e = document.querySelector(".gameboy-text");
-        e.style.animationPlayState = "running";
-        }, 1000);
+        setTimeout(function() {
+            var audio = new Audio("./assets/gameboy_startup_audio.mp3");
+            audio.play();
+            e = document.querySelector(".gameboy-text");
+            e.style.animationPlayState = "running";
+            }, 1000);
+        
+
+        setTimeout(function() {
+            e.style.webkitAnimationName = '';
+            e.style.animationPlayState = "paused";
+            $(".splash-wrapper").fadeOut("slow");
+            }, 4500);
+        
+        displaySplash = false;
+    }
     
-
-    setTimeout(function() {
-        e.style.webkitAnimationName = '';
-        e.style.animationPlayState = "paused";
-        $(".splash-wrapper").fadeOut("slow");
-        }, 4500);
 
 }
 
